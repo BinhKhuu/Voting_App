1b4f3c0a6bd84a578f31842dbebda1b3e10a77b3
BinhKhuu <binhkhuu90@gmail.com>
2018-04-04 16:54:32 +0800

    added listItem structure

--- src/index.js
+++ src/index.js
@@ -5,6 +5,43 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
+class Menubar extends React.Component {
+	render() {
+		return (
+			<div class ="row menuFonts" id="header">
+				<div className="col-8 menuTitle">
+					Title
+				</div>
+				<div className="col-2" id="username">
+					{this.props.username}
+				</div>
+				<div className="col-2">
+					<Dropdown direction='bottom' isOpen={this.props.isOpen} toggle={this.props.toggle}>
+						<DropdownToggle caret id="signin"> login </DropdownToggle>
+						<DropdownMenu right id="dropdownMenu">
+							<DropdownItem id="ddiTwitter">Twitter</DropdownItem>
+							<DropdownItem id="ddiSignin">Sign In</DropdownItem>
+							<DropdownItem id="ddiSignup">Sign Up</DropdownItem>
+						</DropdownMenu>
+					</Dropdown>
+				</div>
+			</div>
+		);
+	}
+}
+
+class ListItem extends React.Component {
+	render() {
+		return (
+			<div className='row item itemFonts'>
+				<div className="col-1" id="itemNum">{this.props.itemNum}</div>
+				<div className="col-10" id="listContainer"><span>{this.props.title}</span></div>
+				<div className="col-1" id="voteCount">{this.props.votes}</div>
+			</div>			
+		);
+	}
+}
+
 class MainPage extends React.Component {
 	constructor(props) {
 		super(props);
@@ -13,6 +50,9 @@ class MainPage extends React.Component {
 		this.state = {
 			dropMenuOpen: false,
 			username: "Guest",
+			title: "Title",
+			votes: 0,
+			itemNum: 1,
 		};
 	}
 	toggleDropMenu() {
@@ -24,32 +64,9 @@ class MainPage extends React.Component {
 		return (
 			<div class='container'>
 				{/*menubar*/}
-				<div class ="row header">
-					<div className="col-8 menuTitle">
-						Title
-					</div>
-					<div className="col-2" id="username">
-						{this.state.username}
-					</div>
-					<div className="col-2">
-						<Dropdown direction='bottom' isOpen={this.state.dropMenuOpen} toggle={this.toggleDropMenu}>
-							<DropdownToggle caret id="signin"> login </DropdownToggle>
-							<DropdownMenu right className="dropdownMenu">
-								<DropdownItem>Twitter</DropdownItem>
-								<DropdownItem>Sign In</DropdownItem>
-								<DropdownItem>Sign Up</DropdownItem>
-							</DropdownMenu>
-						</Dropdown>
-
-					</div>
+				<Menubar username={this.state.username} toggle={this.toggleDropMenu} isOpen={this.state.dropMenuOpen} />
 				{/*end menubar*/}
-				</div>
-				<div className='row voteList'>
-					<div className='col'>
-					{/* add body here*/}
-					</div>
-
-				</div>
+				<ListItem itemNum={this.state.itemNum} title={this.state.title} votes={this.state.votes} />
 				<div className='row footer'>
 					<div className='col'>
 					{/* add footer here*/}
