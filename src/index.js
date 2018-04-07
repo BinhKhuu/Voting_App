67e69bd64ee7112b4127e0b45210c342fd7292d7
BinhKhuu <binhkhuu90@gmail.com>
2018-04-07 16:25:26 +0800

    updated menubar & animation for searchbar

--- src/index.js
+++ src/index.js
@@ -3,30 +3,77 @@ import ReactDOM from 'react-dom';
 /*bootstrap.css interferes with index.css*/
 import './index.css';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-class Menubar extends React.Component {
+import { CSSTransition } from 'react-transition-group';
+
+class NavBar extends React.Component {
 	render() {
 		return (
-			<div class ="row menuFonts" id="header">
-				<div className="col-6 menuTitle">
-					Title
-				</div>
-				<div className="col-2" id="username">
-					{this.props.username}
-				</div>
-				<div className="col-4">
-					<ButtonDropdown direction='bottom' isOpen={this.props.isOpen} toggle={this.props.toggle}>
-						<DropdownToggle caret id="signin">login</DropdownToggle>
-						<DropdownMenu right id="dropdownMenu">
-							<DropdownItem id="ddiTwitter">Twitter</DropdownItem>
-							<DropdownItem id="ddiSignin">Sign In</DropdownItem>
-							<DropdownItem id="ddiSignup">Sign Up</DropdownItem>
-						</DropdownMenu>
-					</ButtonDropdown>
+			<div className="row">
+				<div className="col"id="menubar">
+					<nav className="navbar navbar-expand-sm navbar-dark justify-content-between">
+							{/*Logo on left side*/}
+				  		<a className="navbar-brand" href="javascript:;">Logo</a>
+				  		{/*toggle button on colapse */}
+				  		<div className="navbar-nav">
+					  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"> 
+					  			<span class="navbar-toggler-icon"></span>
+					  		</button>
+							<div className="collapse navbar-collapse" id="collapsibleNavbar">
+								{/*options on right side */}
+								<ul className="navbar-nav">
+									{/*search */}
+									<li className="nav-item">
+										<a className="nav-link" onClick={this.props.toggle}>
+											Search &nbsp;
+											<span className="fa fa-caret-down"></span>
+										</a>
+									</li>
+									{/*username */}
+									<li className="nav-item">
+								    	<a className="nav-link" href="#">username</a>
+								  </li>
+									{/*login dropdown */}
+								  <li className="nav-item dropdown">
+							     	<a className="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+							        Login
+							      	</a>
+							    	<div className="dropdown-menu">
+							        	<a className="dropdown-item" href="#">Link 1</a>
+							        	<a className="dropdown-item" href="#">Link 2</a>
+							        	<a className="dropdown-item" href="#">Link 3</a>
+							      </div>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</nav> 
 				</div>
 			</div>
-		);
+		)
 	}
 }
+
+class SearchBar extends React.Component {
+	render(){
+		return(
+			<div className="row" id="searchbar">
+				<div className="col">
+					<nav className="navbar navbar-dark navbar-center navbar-expand-sm">
+						<ul className="navbar-nav ml-auto">
+							<li className="nav-item">
+								<form class="form-inline menuFonts" action="#">
+									<input class="form-control mr-sm-2" size="40" type="text" placeholder="Search" aria-label="Search"></input>
+									<button class="btn btn-outline-success" type="submit">Search</button>
+								</form>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		)
+	}
+}
+
 
 class ListItem extends React.Component {
 	render() {
@@ -44,9 +91,11 @@ class Footer extends React.Component {
 	render() {
 		return (
 			<div className='row footer menuFonts'>
+				{/*Author */}
 				<div className='col-6'>
 					<p id="author">Binh Khuu</p>
-				</div>
+				</div>	
+				{/*icons on the right */}			
 				<div id="icons" className="col-6">
 					<a href="https://github.com/BinhKhuu/Voting_App" target="_blank"><i id="github" className="fa fa-github" ></i></a>
 					<a href="https://codepen.io/spoonable/#" target="_blank"><i id="codepen" className="fa fa-codepen"></i></a>
@@ -59,30 +108,41 @@ class Footer extends React.Component {
 class MainPage extends React.Component {
 	constructor(props) {
 		super(props);
-		this.toggleDropMenu = this.toggleDropMenu.bind(this);
 		this.state = {
-			dropMenuOpen: false,
+			searchbar: false,
 			username: "Guest",
 			title: "Title",
 			votes: 0,
 			itemNum: 1,
 		};
+		this.toggleSearchBar = this.toggleSearchBar.bind(this);
 	}
-	toggleDropMenu() {
-		this.setState({ 
-			dropMenuOpen: !this.state.dropMenuOpen,
-		});
+	toggleSearchBar(){
+		this.setState({
+			searchbar: !this.state.searchbar,
+		})
 	}
 	render() {
 		return (
-			<div class='container' id="container">
-				{/*menubar*/}
-				<Menubar username={this.state.username} toggle={this.toggleDropMenu} isOpen={this.state.dropMenuOpen} />
-				{/*end menubar*/}
+			<div class='container'>
+				<NavBar toggle={this.toggleSearchBar}/>
+				<CSSTransition 
+					in={this.state.searchbar} 
+					timeout={100}
+					unmountOnExit
+					classNames="slide"
+					onExited= {() => {
+						this.setState({
+							searchbar: false,
+						});
+					}}
+				>
+					<SearchBar />
+				</CSSTransition>
 				<div id="listContainer">
 					<ListItem itemNum={this.state.itemNum} title={this.state.title} votes={this.state.votes} />
 				</div>
-				<Footer />				
+				<Footer />			
 			</div>
 		);
 	}
