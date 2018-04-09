3a69ef18ac10c367ec70fe4fece4007f36eb2bc5
BinhKhuu <binhkhuu90@gmail.com>
2018-04-09 22:20:29 +0800

    changed boostrap navbar to react-strap navbar

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { CSSTransition } from 'react-transition-group';
+import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink} from "reactstrap";
 
 class NavBar extends React.Component {
 	render() {
@@ -13,7 +14,7 @@ class NavBar extends React.Component {
 						{/*Logo on left side*/}
 			  		<a className="navbar-brand" href="javascript:;">Logo</a>
 			  		{/*toggle button on colapse */}
-				  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar"> 
+				  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar" onClick={this.props.collapseBtn}> 
 				  			<span class="navbar-toggler-icon"></span>
 				  	</button>
 						<div className="collapse navbar-collapse" id="collapsibleNavbar">
@@ -45,14 +46,119 @@ class NavBar extends React.Component {
 						</div>
 						{/*serchbar toggling*/}
 				</nav> 
+				{/*aninmate and render searchbar*/}
 				<CSSTransition 
 					in={this.props.searchbar} 
-					timeout={100}
+					timeout={400}
 					unmountOnExit
 					classNames="slide"
+					/*remove padding in the body to align content*/
 					onExited= {() => {
 						this.setState({
 							searchbar: false,
+						},()=>{
+							if(window.innerWidth > 600){
+						 		document.body.classList.remove("bodyPaddingLG");		
+							}
+							else {
+								document.body.classList.remove("bodyPaddingSMSearch");
+							}
+
+						});
+					}}
+					>
+				<SearchBar />
+				</CSSTransition>
+			</div>
+		)
+	}
+}
+
+class Header extends React.Component {
+	render(){
+		return(
+			<div className="container fixed-top" id="menubar">
+				<Navbar color="faded" light>
+					<NavbarBrand href="/" className="mr-auto">Logo</NavbarBrand>
+						<Nav>
+							<NavItem>
+								<NavLink onClick={this.props.toggleSearchBar}>Search</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>username</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>Log in</NavLink>
+							</NavItem>
+						</Nav>
+				</Navbar>
+								{/*aninmate and render searchbar*/}
+				<CSSTransition 
+					in={this.props.searchbar} 
+					timeout={400}
+					unmountOnExit
+					classNames="slide"
+					/*remove padding in the body to align content*/
+					onExited= {() => {
+						this.setState({
+							searchbar: false,
+						},()=>{
+							if(window.innerWidth > 600){
+						 		document.body.classList.remove("bodyPaddingLG");		
+							}
+							else {
+								document.body.classList.remove("bodyPaddingSMSearch");
+							}
+
+						});
+					}}
+					>
+				<SearchBar />
+				</CSSTransition>
+			</div>
+		);
+	}
+}
+
+class HeaderCollapse extends React.Component {
+	render(){
+		return(
+			<div className="container fixed-top" id="menubar">
+				<Navbar color="faded" light>
+					<NavbarBrand href="/" className="mr-auto">Logo</NavbarBrand>
+					<NavbarToggler onClick={this.props.toggle} className="mr-2" />
+					<Collapse isOpen={this.props.collapseOpen} navbar >
+						<Nav navbar>
+							<NavItem>
+								<NavLink onClick={this.props.toggleSearchBar}>Search</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>username</NavLink>
+							</NavItem>
+							<NavItem>
+								<NavLink>Log in</NavLink>
+							</NavItem>
+						</Nav>
+					</Collapse>
+				</Navbar>
+								{/*aninmate and render searchbar*/}
+				<CSSTransition 
+					in={this.props.searchbar} 
+					timeout={400}
+					unmountOnExit
+					classNames="slide"
+					/*remove padding in the body to align content*/
+					onExited= {() => {
+						this.setState({
+							searchbar: false,
+						},()=>{
+							if(window.innerWidth > 600){
+						 		document.body.classList.remove("bodyPaddingLG");		
+							}
+							else {
+								document.body.classList.remove("bodyPaddingSMSearch");
+							}
+
 						});
 					}}
 					>
@@ -127,26 +233,95 @@ class MainPage extends React.Component {
 		super(props);
 		this.state = {
 			searchbar: false,
+			collapseOpen: false,
 			username: "Guest",
 			title: "Title",
 			votes: 0,
 			itemNum: 1,
 		};
 		this.toggleSearchBar = this.toggleSearchBar.bind(this);
+		this.updateDimensions = this.updateDimensions.bind(this);
+		this.collapseBtnPress = this.collapseBtnPress.bind(this);
+	}
+	componentDidMount() {
+		window.addEventListener('resize', this.updateDimensions);
+	}
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.updateDimensions);
+	}
+	updateDimensions() {
+		if(window.innerWidth > 600) {
+			this.setState({
+				searchbar: false,
+				collapseOpen: false,
+			},()=>{
+				document.body.className = "";
+			});
+		}
+		if(window.innerWidth < 600) {
+			this.setState({
+				searchbar: false,
+				collapseOpen: false,
+			},()=>{
+				document.body.className = "";
+			});
+		}
+
 	}
 	toggleSearchBar(){
 		this.setState({
 			searchbar: !this.state.searchbar,
-		},()=> {
-			if(this.state.searchbar) document.body.classList.add("bodyPadding");
-			if(!this.state.searchbar) document.body.classList.remove("bodyPadding");
+		},function() {
+			/*add pading to body to align content*/	
+			if(this.state.searchbar && window.innerWidth > 600) document.body.classList.add("bodyPaddingLG");
+			if(this.state.searchbar && window.innerWidth < 600) document.body.classList.add("bodyPaddingSMSearch");
+			//searchbar not showing is handled in CSSTranslation onExit() in navBar
+
 		});
+	}
+	collapseBtnPress(){
+		if(this.state.collapseOpen) {
+			document.body.className = "";
+			this.setState({
+				collapseOpen: !this.state.collapseOpen,
+				searchbar: false,
+			});
+		}
+		else {
+			this.setState({
+				collapseOpen: !this.state.collapseOpen,
+			},function() {
+				if(!this.state.collapseOpen)document.body.className = "";
+				else document.body.className = "bodyPaddingSM";
+			});
+		}
+
+
+		/*
+		this.setState({
+			collapseOpen: !this.state.collapseOpen,
+		},function() {
+			if(!this.state.collapseOpen){
+				if(!this.state.searchbar) document.body.className = "";
+				else {
+					document.body.className = "";
+					this.setState({
+						searchbar: false,
+					});
+				}
+			} 
+			if(this.state.collapseOpen) document.body.className = "bodyPaddingSM";
+		});*/
+		
 	}
 	render() {
 		return (
 			<div class='container'>
-				<NavBar toggle={this.toggleSearchBar} searchbar={this.state.searchbar}/>
+				{/*<NavBar toggle={this.toggleSearchBar} searchbar={this.state.searchbar} collapseOpen={this.state.collapseOpen} collapseBtn={this.collapseBtnPress}/>*/}
+				{window.innerWidth > 600 && <Header toggle={this.collapseBtnPress} collapseOpen={this.state.collapseOpen} toggleSearchBar={this.toggleSearchBar} searchbar={this.state.searchbar} /> }
+				{window.innerWidth < 600 && <HeaderCollapse toggle={this.collapseBtnPress} collapseOpen={this.state.collapseOpen} toggleSearchBar={this.toggleSearchBar} searchbar={this.state.searchbar} />}
 				<div id="listContainer">
+					<ListItem itemNum={this.state.itemNum} title={this.state.title} votes={this.state.votes} />
 					<ListItem itemNum={this.state.itemNum} title={this.state.title} votes={this.state.votes} />
 				</div>
 				<Footer />			
