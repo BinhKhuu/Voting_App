7cf1cd569323f331f83c1c6cb66ff029e059c131
BinhKhuu <binhkhuu90@gmail.com>
2018-04-10 14:48:16 +0800

    added signin modal

--- src/index.js
+++ src/index.js
@@ -2,27 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 /*bootstrap.css interferes with index.css*/
 import './index.css';
-import { ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem, InputGroup, InputGroupAddon, Input } from 'reactstrap';
+import { ButtonDropdown,Dropdown, DropdownToggle, DropdownMenu, DropdownItem, UncontrolledDropdown, InputGroup, InputGroupAddon, Input } from 'reactstrap';
 import { CSSTransition } from 'react-transition-group';
 import {Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Button, Form, FormGroup} from "reactstrap";
-
+import {Modal, ModalHeader, ModalBody, ModalFooter, Label, Col } from "reactstrap";
 class Header extends React.Component {
 	render(){
 		return(
 			<div className="container fixed-top" id="menubar">
 				<Navbar color="faded" dark>
 					<NavbarBrand href="/" className="mr-auto">Logo</NavbarBrand>
-						<Nav className="menuFonts">
-							<NavItem>
-								<NavLink onClick={this.props.toggleSearchBar}>Search &nbsp;<span className="fa fa-caret-down"></span></NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink>username</NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink>Log in</NavLink>
-							</NavItem>
-						</Nav>
+					<Nav className="menuFonts">
+						<NavItem className="menuSearch">
+							<NavLink 
+								onClick={this.props.toggleSearchBar}
+							> Search &nbsp;<span className="fa fa-caret-down"></span>
+							</NavLink>
+						</NavItem>
+						<NavItem>
+							<NavLink>username</NavLink>
+						</NavItem>
+						<Dropdown 
+							isOpen={this.props.dropOpen} 
+							toggle={this.props.toggleDropdown}
+							nav
+						>
+	                	<DropdownToggle className="menuFonts" nav caret>Log In</DropdownToggle>
+	                	<DropdownMenu right>
+		                  <DropdownItem id="ddiTwitter">Twitter</DropdownItem>
+		                  <DropdownItem onClick={this.props.toggleSignIn} id="ddiSignin">Sign In</DropdownItem>
+		                  <DropdownItem id="ddiSignup">SignUP</DropdownItem>
+	               	</DropdownMenu>
+	              	</Dropdown>
+					</Nav>
 				</Navbar>
 				{/*aninmate and render searchbar*/}
 				<CSSTransition 
@@ -60,15 +72,24 @@ class HeaderCollapse extends React.Component {
 					<NavbarToggler onClick={this.props.toggle} className="mr-2" />
 					<Collapse isOpen={this.props.collapseOpen} navbar>
 						<Nav className="menuFonts" navbar>
-							<NavItem>
+							<NavItem className="menuSearch">
 								<NavLink onClick={this.props.toggleSearchBar}>Search &nbsp;<span className="fa fa-caret-down"></span></NavLink>
 							</NavItem>
 							<NavItem>
 								<NavLink>username</NavLink>
 							</NavItem>
-							<NavItem>
-								<NavLink>Log in</NavLink>
-							</NavItem>
+							<Dropdown 
+								isOpen={this.props.dropOpen} 
+								toggle={this.props.toggleDropdown}
+								nav
+							>
+		                	<DropdownToggle className="menuFonts" nav caret>Log In</DropdownToggle>
+		                	<DropdownMenu right>
+			                  <DropdownItem id="ddiTwitter">Twitter</DropdownItem>
+			                  <DropdownItem onClick={this.props.toggleSignIn} id="ddiSignin">Sign In</DropdownItem>
+			                  <DropdownItem id="ddiSignup">SignUP</DropdownItem>
+		               	</DropdownMenu>
+		              	</Dropdown>
 						</Nav>
 					</Collapse>
 				</Navbar>
@@ -107,10 +128,10 @@ class Searchbar extends React.Component {
 					<Navbar>
 						<Nav className="ml-auto">
 							<NavItem>		
-					        <InputGroup>
-					        	<Input type="text" size="40" placeholder="search..."/>
-					          <InputGroupAddon addonType="append"><Button type="submit">Search</Button></InputGroupAddon> 
-					        </InputGroup>
+					        	<InputGroup>
+					        		<Input type="text" size="40" placeholder="search..."/>
+					          	<InputGroupAddon addonType="append"><Button type="submit">Search</Button></InputGroupAddon> 
+					        	</InputGroup>
 							</NavItem>
 						</Nav>
 					</Navbar>
@@ -129,6 +150,41 @@ class ListItem extends React.Component {
 				<div className="col-1" id="voteCount">{this.props.votes}</div>
 			</div>			
 		);
+	}
+}
+
+class SignInModal extends React.Component {
+	render(){
+		return (
+			<div>
+				<Modal isOpen={this.props.isOpen} toggle={this.props.toggleSignIn}>
+					<ModalHeader toggle={this.props.toggleSignIn}>Sign In</ModalHeader>
+					<ModalBody>
+						<Form action="#soon">
+							<FormGroup>
+									<InputGroup>
+									<InputGroupAddon addonType="prepend">
+										Email
+										&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+									</InputGroupAddon>
+						        		<Input type="text" placeholder="Someone@email.com"/>	 
+						        	</InputGroup>
+							</FormGroup>
+							<FormGroup>
+									<InputGroup>
+									<InputGroupAddon addonType="prepend">Password</InputGroupAddon>
+						        		<Input type="text" placeholder="******"/>	 
+						        	</InputGroup>
+							</FormGroup>
+						</Form>
+					</ModalBody>
+					<ModalFooter>
+						<Button color="success" type="submit">Sign In</Button>
+						<Button color="secondary">Sign Up</Button>
+					</ModalFooter>
+				</Modal>
+			</div>
+		)
 	}
 }
 
@@ -154,16 +210,20 @@ class MainPage extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
+			menuDropdown: false,
 			searchbar: false,
 			collapseOpen: false,
+			signInModal: false,
 			username: "Guest",
 			title: "Title",
 			votes: 0,
 			itemNum: 1,
 		};
 		this.toggleSearchBar = this.toggleSearchBar.bind(this);
+		this.toggleSignIn = this.toggleSignIn.bind(this);
 		this.updateDimensions = this.updateDimensions.bind(this);
 		this.collapseBtnPress = this.collapseBtnPress.bind(this);
+		this.toggleDropdown = this.toggleDropdown.bind(this);
 	}
 	componentDidMount() {
 		window.addEventListener('resize', this.updateDimensions);
@@ -218,17 +278,33 @@ class MainPage extends React.Component {
 			});
 		}
 	}
+	toggleDropdown(){
+		this.setState({
+			menuDropdown: !this.state.menuDropdown,
+		});
+	}
+	toggleSignIn(){
+		this.setState({
+			signInModal: !this.state.signInModal,
+		});
+	}
 	render() {
 		return (
 			<div class='container'>
 				{	window.innerWidth > 600 && <Header 
-															toggle={this.collapseBtnPress} 
+															toggleSignIn={this.toggleSignIn}
+															dropOpen={this.state.menuDropdown}
+															toggleDropdown={this.toggleDropdown}
+
 															collapseOpen={this.state.collapseOpen} 
 															toggleSearchBar={this.toggleSearchBar} 
 															searchbar={this.state.searchbar} 
 														/> 
 				}
 				{	window.innerWidth < 600 && <HeaderCollapse 
+															toggleSignIn={this.toggleSignIn}
+															dropOpen={this.state.menuDropdown}
+															toggleDropdown={this.toggleDropdown}
 															toggle={this.collapseBtnPress} 
 															collapseOpen={this.state.collapseOpen} 
 															toggleSearchBar={this.toggleSearchBar} 
@@ -247,6 +323,10 @@ class MainPage extends React.Component {
 						votes={this.state.votes} 
 					/>
 				</div>
+				<SignInModal 
+					isOpen={this.state.signInModal}
+					toggleSignIn={this.toggleSignIn}
+				/>
 				<Footer />			
 			</div>
 		);
